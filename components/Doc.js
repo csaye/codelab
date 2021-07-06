ea2666b401d17e1192c01980fc80251e460ab21d
Cooper Saye <coopersaye@gmail.com>
2021-07-05 23:34:07 -0700

    Integrate Doc with firebase.

--- components/Doc.js
+++ components/Doc.js
@@ -1,15 +1,39 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-import styles from '../styles/Doc.module.css';
+import firebase from 'firebase/app';
 
 export default function Doc() {
   const [text, setText] = useState(undefined);
+
+  const docRef = firebase.firestore().collection('docs').doc('doc');
+
+  // updates text locally and in firebase
+  async function updateText(newText) {
+    setText(newText);
+    await docRef.update({ text: newText });
+  }
+
+  // retrieves text from firebase
+  async function getText() {
+    const doc = await docRef.get();
+    setText(doc.data().text);
+  }
+
+  // get text on start
+  useEffect(() => {
+    getText();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  // if no text, return loading
+  if (text === undefined) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <div>
       <textarea
         value={text}
-        onChange={e => setText(e.target.value)}
+        onChange={e => updateText(e.target.value)}
       />
     </div>
   );
