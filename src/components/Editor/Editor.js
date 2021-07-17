4655ab106323446ead337dc3c03fdd3aacfedc9c
Cooper Saye <coopersaye@gmail.com>
2021-07-16 22:25:39 -0700

    Improve editor styling.

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -30,40 +30,46 @@ export default function Editor(props) {
   }, [html, css, js]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <div>
-      <h1>HTML</h1>
-      <Controlled
-        value={html}
-        onBeforeChange={(e, d, v) => setHtml(v)}
-        options={{
-          mode: 'xml',
-          lineWrapping: true,
-          theme: 'material',
-          lineNumbers: true
-        }}
-      />
-      <h1>CSS</h1>
-      <Controlled
-        value={css}
-        onBeforeChange={(e, d, v) => setCss(v)}
-        options={{
-          mode: 'css',
-          lineWrapping: true,
-          theme: 'material',
-          lineNumbers: true
-        }}
-      />
-      <h1>JavaScript</h1>
-      <Controlled
-        value={js}
-        onBeforeChange={(e, d, v) => setJs(v)}
-        options={{
-          mode: 'javascript',
-          lineWrapping: true,
-          theme: 'material',
-          lineNumbers: true
-        }}
-      />
+    <div className={styles.container}>
+      <div className={styles.window}>
+        <h1>HTML</h1>
+        <Controlled
+          value={html}
+          onBeforeChange={(e, d, v) => setHtml(v)}
+          options={{
+            mode: 'xml',
+            lineWrapping: true,
+            theme: 'material',
+            lineNumbers: true
+          }}
+        />
+      </div>
+      <div className={styles.window}>
+        <h1>CSS</h1>
+        <Controlled
+          value={css}
+          onBeforeChange={(e, d, v) => setCss(v)}
+          options={{
+            mode: 'css',
+            lineWrapping: true,
+            theme: 'material',
+            lineNumbers: true
+          }}
+        />
+      </div>
+      <div className={styles.window}>
+        <h1>JS</h1>
+        <Controlled
+          value={js}
+          onBeforeChange={(e, d, v) => setJs(v)}
+          options={{
+            mode: 'javascript',
+            lineWrapping: true,
+            theme: 'material',
+            lineNumbers: true
+          }}
+        />
+      </div>
     </div>
   );
 }
